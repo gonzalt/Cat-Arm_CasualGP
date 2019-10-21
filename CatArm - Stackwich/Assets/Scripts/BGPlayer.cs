a4f80ce033016f901395e9bb5b8c0fd33b31ff16
Tyler Gonzalez <gonzalt15@gmail.com>
2019-10-21 00:08:56 -0400

    Implemented Animated Background

---
+++ CatArm - Stackwich/Assets/Scripts/BGPlayer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGPlayer : MonoBehaviour
+{
+
+	public GameObject videoHolder;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+
+		//videoHolder.GetComponent<VideoPlayer>().Play();
+
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
