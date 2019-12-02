79bc506050417b948d28ef841dee8e54447df132
Tyler Gonzalez <gonzalt15@gmail.com>
2019-12-01 22:38:23 -0500

    How To Scene Image Changing

---
+++ CatArm - Stackwich/Assets/UI Elements/DestroyGOs.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyGOs : MonoBehaviour
+{
+
+
+	//public GameObject targetOne;
+	//public GameObject targetTwo;
+	//public GameObject targetThree;
+
+
+	public void DestroyUISet(GameObject targetOne, GameObject targetTwo, GameObject targetThree)
+	{
+		Destroy(targetOne);
+		Destroy(targetTwo);
+		Destroy(targetThree);
+	}
+
+
+
+
+
+
+
+
+
+}
